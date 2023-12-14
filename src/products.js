46cecdcaecb39637a96e379c6d218b32ff0de266
aswanth2612 <aswanthmmg@gmail.com>
2023-12-14 12:16:23 +0530

    first commit

---
+++ src/products.js
@@ -0,0 +1,31 @@
+import product1 from './assets//1.png';
+import product2 from './assets//2.png';
+import product3 from './assets/3.png';
+import product4 from './assets/4.png';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "IPhone",
+        price: 1200,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "MacBook",
+        price: 1700,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Camera",
+        price: 1500,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Shirt",
+        price: 500,
+        productImage: product4,
+    },
+];
